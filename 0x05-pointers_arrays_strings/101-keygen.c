cdc7a9aff2a7785ede7e32805414568b33483a9a
heniam21 <henokdesalegn1995@gmail.com>
2022-03-22 09:35:10 -0700

    05

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
+ * main - creates a password of sum 2772
+ *
+ * Return:0;
  */
 int main(void)
 {
-int num;
+	int keynumber, symbol, checksum;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
-return (0);
+	srand(time(NULL));
+	checksum = 2772;
+	keynumber = 0;
+
+	while (keynumber < (checksum - 122))
+	{
+		symbol = (rand() % (122 - 97 + 1)) + 97;/*to print mostly lowercaseletters*/
+		printf("%c", symbol);
+		keynumber = keynumber + symbol;
+	}
+
+	symbol = checksum - keynumber;
+	printf("%c", symbol);
+
+	return (0);
 }
